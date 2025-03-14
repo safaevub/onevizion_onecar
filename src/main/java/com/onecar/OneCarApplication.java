e6617c9483e2e865ecc5831aadc954450959a62e
Utkurbek Safaev <safaevub@gmail.com>
2025-03-14 10:32:14 +0500

    finished task
    Decomposed Technical Description

---
+++ src/main/java/com/onecar/OneCarApplication.java
@@ -0,0 +1,11 @@
+package com.onecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneCarApplication.class, args);
+    }
+}
